60a30ea698ad359b577da9179dc060a8c3c7ba06
Egor Sobolev <egor.sobolev@gmail.com>
2026-01-03 17:15:43 +0100

    Change the format of the command line

--- src/rism/conv/__init__.py
+++ src/rism/conv/__init__.py
@@ -10,15 +10,15 @@ def main():
     converters = get_converters()
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--rtxt", type=Path,
+                        help="RISM molecule file")
+
     subparsers = parser.add_subparsers(dest='format', required=True,
                                        help='format-specific help')
 
     for frm, conv_cls in converters.items():
         ap = subparsers.add_parser(frm, help=conv_cls.help)
         conv_cls.arguments(ap)
-
-    parser.add_argument("rtxt", type=Path, nargs='?',
-                        help="RISM molecule file")
 
     args = parser.parse_args()
     conv = converters[args.format](args)
